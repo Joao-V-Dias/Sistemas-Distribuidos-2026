a8a3fb1801339bbf28be244e5a74ae0730437ff7
Joao Dias <100037973+Joao-V-Dias@users.noreply.github.com>
2026-05-03 14:36:29 -0300

    Add initial Spring Boot project prj_planta_serv_1

---
+++ prj_planta_serv_1/src/main/java/com/sd/prj_planta_serv_1/models/Flor.java
@@ -0,0 +1,101 @@
+package com.sd.prj_planta_serv_1.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "flor")
+public class Flor implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int idflor;
+
+    @Column(nullable = false)
+    private double comprimento_sepala;
+
+    @Column(nullable = false)
+    private double largura_sepala;
+
+    @Column(nullable = false)
+    private double comprimento_petala;
+
+    @Column(nullable = false)
+    private double largura_petala;
+
+    @Column(nullable = false)
+    private String cor;
+
+    @Column(nullable = false)
+    private String especieTipo;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idplanta", nullable = false)
+    private Planta planta;
+
+    public Planta getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(Planta planta) {
+        this.planta = planta;
+    }
+
+    public int getIdflor() {
+        return idflor;
+    }
+
+    public void setIdflor(int idflor) {
+        this.idflor = idflor;
+    }
+
+    public double getComprimento_sepala() {
+        return comprimento_sepala;
+    }
+
+    public void setComprimento_sepala(double comprimento_sepala) {
+        this.comprimento_sepala = comprimento_sepala;
+    }
+
+    public double getLargura_sepala() {
+        return largura_sepala;
+    }
+
+    public void setLargura_sepala(double largura_sepala) {
+        this.largura_sepala = largura_sepala;
+    }
+
+    public double getComprimento_petala() {
+        return comprimento_petala;
+    }
+
+    public void setComprimento_petala(double comprimento_petala) {
+        this.comprimento_petala = comprimento_petala;
+    }
+
+    public double getLargura_petala() {
+        return largura_petala;
+    }
+
+    public void setLargura_petala(double largura_petala) {
+        this.largura_petala = largura_petala;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getEspecieTipo() {
+        return especieTipo;
+    }
+
+    public void setEspecieTipo(String especieTipo) {
+        this.especieTipo = especieTipo;
+    }
+}
---
+++ prj_planta_serv_1/src/main/java/com/sd/prj_planta_serv_1/models/Planta.java
@@ -0,0 +1,49 @@
+package com.sd.prj_planta_serv_1.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "planta")
+public class Planta implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int idplanta;
+
+    @Column(nullable = false, unique = true)
+    private String genero;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "planta", fetch = FetchType.LAZY)
+    Set<Flor> flores = new HashSet<Flor>();
+
+    public int getIdplanta() {
+        return idplanta;
+    }
+
+    public void setIdplanta(int idplanta) {
+        this.idplanta = idplanta;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Set<Flor> getFlores() {
+        return flores;
+    }
+
+    public void setFlores(Set<Flor> flores) {
+        this.flores = flores;
+    }
+}
